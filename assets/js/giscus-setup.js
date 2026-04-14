a89551b7a6d78544e8a2f41e39406c8a9b80f7f0
vlmathieu <valentin.mathieu25@gmail.com>
2026-04-14 07:14:49 +0000

    Deploying to gh-pages from @ vlmathieu/vlmathieu.github.io@2268d0b1e4cd6b8416f50368dced3f1f1dea26ee 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "vlmathieu/lvlmathieu-new.github.io",
+    "data-repo": "vlmathieu/vlmathieu.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
