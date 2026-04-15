531cecb47c15149870aa17582e99e294f2905526
vlmathieu <valentin.mathieu25@gmail.com>
2026-04-15 16:50:37 +0000

    Deploying to gh-pages from @ vlmathieu/vlmathieu.github.io@bf24ca345a8ce267c78055511146b0417ce4c86a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "In progress GitHub repositories on a journey to open science.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
