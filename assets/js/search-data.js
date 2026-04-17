c3041ec7f0e292e78e0599345e861816650fb260
vlmathieu <valentin.mathieu25@gmail.com>
2026-04-17 09:44:00 +0000

    Deploying to gh-pages from @ vlmathieu/vlmathieu.github.io@5803f6ce3edf0b545f15486ef72b6d2ce877a07f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -468,9 +468,9 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
+            },},{id: "teachings-ue-filière-forêt-bois-et-commercialisation-des-produits-forestiers",
+          title: 'UE Filière Forêt-Bois et Commercialisation des Produits Forestiers',
+          description: "Cette unité d&#39;enseignement sensibilise les étudiants à différentes dimensions du secteur forêt-bois. Elle aborde à la fois des aspects opérationnels (commercialisation, cartographie des acteurs, visites d&#39;industries) et stratégiques (commerce international).",
           section: "Teachings",handler: () => {
               window.location.href = "/teachings/data-science-fundamentals/";
             },},{id: "teachings-introduction-to-machine-learning",
